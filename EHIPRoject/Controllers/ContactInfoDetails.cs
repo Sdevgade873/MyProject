511ec530f8e87e64cb27e51a17018802db8f33cf
Sdevgade873 <43208354+Sdevgade873@users.noreply.github.com>
2019-11-07 20:17:19 +0530

    Add files via upload

---
+++ EHIPRoject/Controllers/ContactInfoDetails.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EHIProject.Models;
+using EHIProject.Interface;
+
+namespace EHIPRoject.Controllers
+{
+    public class ContactInfoDetails : ApiController
+    {
+        // GET api/values
+        private IContacts _contact;
+        private MemberInfo _contactdetails;
+
+        public ContactInfoDetails(IContacts contact,MemberInfo contactdetails)
+        {
+            _contact = contact;
+            _contactdetails = contactdetails;
+        }
+
+        // GET api/values/5
+        public MemberInfo GetByID(int id)
+        {
+            // MemberInfo  = new MemberInfo();
+            _contactdetails = _contact.ContactByID(id);
+
+            return _contactdetails;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void SaveContact([FromBody]MemberInfo value)
+        {
+            _contact.SaveContact(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public void Put([FromBody]MemberInfo value)
+        {
+            _contact.UpdateContact(value);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            _contact.DeleteContact(id);
+        }
+    }
+}
---
+++ EHIPRoject/DBConcreate/MemberInfoContext.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.Entity;
+//using System.Linq;
+//using System.Web;
+
+//namespace EHIPRoject.DBConcreate
+//{
+//    public class DatabaseContext : DbContext
+//    {
+
+//        public DbSet<Registration> Registration { get; set; }
+//        public DbSet<Country> Country { get; set; }
+//        public DbSet<City> City { get; set; }
+//        public DbSet<States> States { get; set; }
+//        public DbSet<Roles> Roles { get; set; }
+//        public DbSet<Venue> Venue { get; set; }
+//        public DbSet<Equipment> Equipment { get; set; }
+//        public DbSet<Food> Food { get; set; }
+//        public DbSet<Dishtypes> Dishtypes { get; set; }
+//        public DbSet<Light> Light { get; set; }
+//        public DbSet<Flower> Flower { get; set; }
+//        public DbSet<BookingDetails> BookingDetails { get; set; }
+//        public DbSet<BookingVenue> BookingVenue { get; set; }
+//        public DbSet<Event> Event { get; set; }
+//        public DbSet<BookingEquipment> BookingEquipment { get; set; }
+//        public DbSet<BookingFood> BookingFood { get; set; }
+//        public DbSet<BookingFlower> BookingFlower { get; set; }
+//        public DbSet<BookingLight> BookingLight { get; set; }
+
+//    }
+//}
