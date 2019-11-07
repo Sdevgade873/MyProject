35eec88f909c728af5ee9f20e7fe801bdbd476db
Sdevgade873 <43208354+Sdevgade873@users.noreply.github.com>
2019-11-07 19:58:16 +0530

    DataConcreate

---
+++ EHIProject.DataConcreate/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using EHIProject.Models;
+
+namespace EHIProject.DataConcreate
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base()
+        {
+
+        }
+
+        public DbSet<MemberInfo> dataMemberInfo { get; set; }
+       
+
+    }
+
+    public class DbContextOptions<T>
+    {
+    }
+}
