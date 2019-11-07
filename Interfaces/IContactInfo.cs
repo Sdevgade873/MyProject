38e9bc05d54ccc521280e3800641388ef67e952f
Sdevgade873 <43208354+Sdevgade873@users.noreply.github.com>
2019-11-07 20:19:00 +0530

    Add files via upload

---
+++ Interfaces/IContactInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EHIPRoject.Models;
+
+
+namespace EHIProject.Interfaces
+{
+    public interface IGetContact
+    {
+        void SaveContact(MemberInfo info);
+        void UpdateContact(MemberInfo info);
+        IQueryable<MemberInfo> ShowContact(string sortColumn, string sortColumnDir, string Search);
+        IEnumerable<MemberInfo> ShowAllContact();
+        int DeleteContact(int id);
+        MemberInfo ContactByID(int id);
+        bool CheckContactNameAlready(string venueName);
+    }
+}
