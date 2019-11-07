b9ba58ab7ba96eca914b0916b924d55e2ee777ef
Sdevgade873 <43208354+Sdevgade873@users.noreply.github.com>
2019-11-07 19:59:34 +0530

    Interface

---
+++ EHIProject.Interface/IContactInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EHIProject.Models;
+
+namespace EHIProject.Interface
+{
+    public interface IContacts
+    {
+        void SaveContact(MemberInfo info);
+        void UpdateContact(MemberInfo info);
+        IEnumerable<MemberInfo> ShowAllContact();
+        int DeleteContact(int id);
+        MemberInfo ContactByID(int id);
+        bool CheckContactNameAlready(Guid Name);
+    }
+}
